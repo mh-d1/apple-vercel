6f00d53d9402085ec13573776ad139e081295312
Mn <mmhdhnfii@gmail.com>
2026-05-14 04:28:51 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -22,16 +22,7 @@ self.addEventListener("install", event => {
 
 });
 
-self.addEventListener("fetch", event => {
-
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
-  );
-
-});
 self.addEventListener("install", () => {
   console.log("Service Worker Installed");
 });
+
